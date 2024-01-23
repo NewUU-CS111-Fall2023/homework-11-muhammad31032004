1c609d5a80333fc12c75b96e47a373a24383c3fd
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2024-01-23 20:39:18 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -1,9 +1,49 @@
 /*
- * Author:
- * Date:
- * Name:
+
+ * Author: Mukhammadkhon Asrorov 
+
+ * Date: 23/01/2024
+
+ * Name: HW -11
+
  */
 
-class Problem7 {
-public:
-};
+
+
+#include <iostream>
+
+
+
+int phi(int n) {
+
+    if (n == 1) return 0;
+
+    for (int i = 2; i * i <= n; ++i) {
+
+        if (n % i == 0) return phi(n / i) * (i - 1);
+
+    }
+
+    return n - 1;
+
+}
+
+
+
+int main() {
+
+    int a = 13;
+
+    int b = 17;
+
+    int n = a * b;
+
+    int phi_n = phi(n);
+
+    std::cout << "phi(" << n << ") = " << phi_n << std::endl;
+
+    return 0;
+
+}
+
+ 
