aee0e7c539906149f7a352e6169518545089a6fd
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2024-01-23 20:28:00 +0500

    Update task_1.cpp

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -1,9 +1,55 @@
-/*
- * Author:
- * Date:
- * Name:
+ /*
+ * Author: Mukhammadkhon Asrorov 
+ * Date: 23/01/2024
+ * Name: HW -11
  */
 
-class Problem1 {
-public:
-};
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findTargetSumWays(vector<int>& nums, int target) {
+    int sum = 0;
+    for (int num : nums) {
+        sum += num;
+    }
+
+
+    if ((sum + target) % 2 != 0) {
+        return 0;
+    }
+
+    int subsetSum = (sum + target) / 2;
+    int n = nums.size();
+    vector<vector<int>> dp(n + 1, vector<int>(subsetSum + 1, 0));
+
+
+    dp[0][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        dp[i][0] = 1;
+    }
+
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= subsetSum; j++) {
+            if (nums[i - 1] <= j) {
+
+                dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+            } else {
+
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[n][subsetSum];
+}
+
+int main() {
+    vector<int> nums = {1, 1, 1, 1, 1};
+    int target = 3;
+    int ways = findTargetSumWays(nums, target);
+    cout << "Number of ways: " << ways << endl;
+    return 0;
+}
